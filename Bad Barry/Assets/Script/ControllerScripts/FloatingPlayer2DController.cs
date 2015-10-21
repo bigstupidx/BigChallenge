d8ed09cc5d8cc9910d50357211744400028cf612
guilherme babugia <babugia@icloud.com>
2015-10-21 14:04:26 -0200

    joystick

---
+++ Bad Barry/Assets/Script/ControllerScripts/FloatingPlayer2DController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class FloatingPlayer2DController : MonoBehaviour {
+
+	public float moveForce = 5;
+	Rigidbody2D myBody;
+
+	// Use this for initialization
+	void Start () {
+		myBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+		Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+
+		myBody.AddForce (moveVec);
+
+	}
+}
