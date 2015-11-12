a2e4430718e289b7f38a8d72881796703e90b0be
guilherme babugia <babugia@icloud.com>
2015-11-12 16:12:34 -0200

    ínventario

---
+++ Bad Barry/Assets/Script/InventoryScripts/Inventory.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+
+	GameObject inventoryPanel;
+	GameObject slotPanel;
+	ItemDatabase database;
+	public GameObject inventorySlot;
+	public GameObject inventoryItem;
+
+	GameObject Options;
+
+	int slotAmount;
+	public List<Item> items = new List<Item> ();
+	public List<GameObject> slots = new List<GameObject> ();
+	public List<GameObject> itemsList = new List<GameObject> ();
+
+	void Start(){
+		database = GetComponent<ItemDatabase> ();
+
+		slotAmount = 18;
+		inventoryPanel = GameObject.Find ("inventory Panel");
+		slotPanel = inventoryPanel.transform.FindChild ("slot Panel").gameObject;
+
+		Options = GameObject.Find("Options");
+
+
+
+		for (int i = 0; i < slotAmount; i++) {
+			items.Add (new Item());
+			slots.Add (Instantiate(inventorySlot));
+			slots[i].GetComponent<Slot>().id = i;
+			slots[i].transform.SetParent (slotPanel.transform);
+
+		}
+
+		AddItem (0);
+		AddItem (0);
+		AddItem (1);
+		AddItem (1);
+		AddItem (1);
+		AddItem (2);
+
+	}
+
+	public void AddItem(int id){
+		Item itemToAdd = database.FetchItemByID (id);
+		if (itemToAdd.Stackable && CheckIfItemIsInventory (itemToAdd)) {
+			for (int i=0; i< items.Count; i++) {
+				if (items [i].ID == id) {
+					ItemData data = slots [i].transform.GetChild (0).GetComponent<ItemData> ();
+					data.amount++;
+					data.transform.GetChild (0).GetComponent<Text> ().text = data.amount.ToString ();
+					break;
+				}
+			}
+		} else if(!CheckIfItemIsInventory(itemToAdd)) {
+
+			for (int i=0; i<items.Count; i++) {
+				if (items [i].ID == -1) {
+					items [i] = itemToAdd;
+					GameObject itemObj = Instantiate (inventoryItem);
+					itemObj.GetComponent<ItemData>().item = itemToAdd;
+					itemObj.GetComponent<ItemData>().slot = i;
+					itemObj.transform.SetParent (slots [i].transform);
+					itemObj.transform.position = Vector2.zero;
+					itemObj.GetComponent<Image> ().sprite = itemToAdd.Sprite;
+					itemObj.name = itemToAdd.Title;
+					itemsList.Add(itemObj);
+
+					break;
+				}
+			}
+		}
+	}
+
+	
+	public void DecrementItem(int id){
+			for (int i=0; i< items.Count; i++) {
+				if (items [i].ID == id) {
+					ItemData data = slots [i].transform.GetChild (0).GetComponent<ItemData> ();
+					
+				if(data.amount == 0)
+					RemoveItem(id);
+
+				if(data.amount > 0)
+					data.amount--;
+				//CHAMAR FUNCAO PARA AUMENTAR A VIDA IN GAME
+
+
+				data.transform.GetChild (0).GetComponent<Text> ().text = data.amount.ToString ();
+					break;
+				}
+			}
+	}
+
+	public void RemoveItem(int id){
+		for (int i=0; i<items.Count; i++) {
+			if (items [i].ID == id) {
+
+				for(int j=0; j<itemsList.Count; j++){
+
+					Debug.Log(itemsList[j].name);
+
+					if(itemsList[j].name == items[i].Title){
+						Destroy(itemsList[j]);
+						itemsList.RemoveAt(j);
+					}
+				}
+
+				Debug.Log ("Entrou para remover");
+				
+				break;
+			}
+		}
+	}
+
+	bool CheckIfItemIsInventory(Item item){
+
+		for (int i=0; i< items.Count; i++) 
+			if(items[i].ID == item.ID)
+				return true;
+		return false;
+
+	}
+}
---
+++ Bad Barry/Assets/Script/InventoryScripts/ItemData.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler,
+IPointerEnterHandler, IPointerExitHandler, IEventSystemHandler, ISelectHandler,IDeselectHandler, IPointerClickHandler {
+
+	public Item item;
+	public int amount;
+	public int slot;
+
+	private Inventory inv;
+	private Tooltip tooltip;
+	private Vector2 offset;
+
+	private Options options;
+
+	//Options options;
+
+
+
+	void Start(){
+		inv = GameObject.Find ("Inventory").GetComponent<Inventory> ();
+		tooltip = inv.GetComponent<Tooltip> ();
+		options = inv.GetComponent<Options> ();
+		options.Deactivate ();
+	}
+	
+
+	public void OnSelect(BaseEventData eventData){
+		Debug.Log ("CLIQUEI");
+
+		//options.Activate(item, this.transform.position);
+		//options.transform.SetParent (this.transform);
+
+	}
+
+	public void OnDeselect(BaseEventData eventData){
+		Debug.Log ("DESCLIQUEI");
+		//options.Deactivate ();
+	}
+
+	public void OnPointerClick(PointerEventData eventData){
+		Debug.Log ("cliquei");
+		if (item.Stackable)
+			inv.DecrementItem (item.ID);
+		//else
+			//inv.EquipItem (item.ID); // CASO FOR UMA ARMA, IR PARA UMA FUNCAO PARA EQUIPA-LA NO PERSONAGEM
+	}
+
+	public void OnBeginDrag(PointerEventData eventData){
+		if(item != null){
+			offset = new Vector2(0,0);
+			this.transform.SetParent(this.transform.parent.parent); // setando o parente do item para o slotpanel 
+			this.transform.position = eventData.position - offset;
+			GetComponent<CanvasGroup>().blocksRaycasts = false;
+		}
+	}
+
+	public void OnDrag(PointerEventData eventData){
+		if(item != null){
+			this.transform.position = eventData.position - offset;
+		}
+
+	}
+
+	public void OnEndDrag(PointerEventData eventData){
+
+		this.transform.SetParent(inv.slots[slot].transform);
+		this.transform.position = inv.slots[slot].transform.position;
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+	}
+
+	public void OnPointerDown(PointerEventData eventData){
+		if (item != null) {
+			offset = eventData.position - new Vector2 (this.transform.position.x, this.transform.position.y);
+			
+			// Selection tracking
+			EventSystem.current.SetSelectedGameObject(gameObject, eventData);
+
+		}
+
+
+
+	}
+
+	public void OnPointerEnter(PointerEventData eventData){
+		tooltip.Activate (item);
+	}
+
+	public void OnPointerExit(PointerEventData eventData){
+		tooltip.Deactivate ();
+	}
+
+
+}
+ 
