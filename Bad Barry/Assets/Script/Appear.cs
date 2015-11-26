a041dc3a0c7fc0a2318fc293cd5379a7a1c46b41
HyagoHirai <Hyago1@MacBook-Pro-de-Hyago.local>
2015-11-26 17:05:22 -0200

    fix

---
+++ Bad Barry/Assets/Script/Appear.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Appear : MonoBehaviour {
+	
+	public GameObject[] floors;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void AppearThing()
+	{
+		foreach (GameObject floor in floors) {
+			floor.SetActive(true);
+		}
+	}
+	
+	void OnTriggerEnter2D (Collider2D col){
+		print ("Bateu");
+		if (col.gameObject.tag == "Player") {
+			print (floors.Length);
+			foreach (GameObject floor in floors) {
+				floor.SetActive(true);
+			}
+		}
+	}
+	
+}
---
+++ Bad Barry/Assets/Script/Score.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	int score = 0;
+	Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+	
+		scoreText = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void reset(){
+
+		score = 0;
+		scoreText.text = score.ToString();
+
+	}
+	public void inc(){
+	
+		score += 100;
+		scoreText.text = score.ToString();
+
+	
+	}
+}
