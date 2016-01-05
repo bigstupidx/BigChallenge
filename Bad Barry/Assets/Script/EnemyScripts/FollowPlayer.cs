aae98c888368bcf2cf3ab049f06e6b8bc55b2458
Neves <nevesferreira.gabriel@hotmail.com>
2016-01-05 14:15:20 -0200

    AI

--- Bad Barry/Assets/Script/EnemyScripts/FollowPlayer.cs
+++ Bad Barry/Assets/Script/EnemyScripts/FollowPlayer.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class FollowPlayer : MonoBehaviour {
 
+	public bool active = false;
+
 	public	float speed;
 	public  float rangeMin = 2; 
 	public float rangeMax = 100; 
-	private int direction = 2;
+	public int direction = 0;
 
 	private Transform player;
 
@@ -20,6 +22,8 @@ public class FollowPlayer : MonoBehaviour {
 
 	public float distanceY;
 	public float distanceX;
+	public float rangeShoot;
+
 	
 
 
@@ -37,133 +41,143 @@ public class FollowPlayer : MonoBehaviour {
 		bothAnimator = transform.GetComponent<Animator> ();
 		legAnimator = transform.Find("Legs").GetComponent<Animator>();
 		torsoAnimator = transform.Find("Torso").GetComponent<Animator>();
+		gameObject.GetComponent<Enemy> ().shootDirection = direction;
+		bothAnimator.SetInteger ("direction", direction);
+		torsoAnimator.SetInteger ("Direction", direction);
+		legAnimator.SetInteger ("Direction", direction);
 	}
 	
 	void Update (){
 
-		var behave = GameObject.FindGameObjectWithTag("Behaviour").GetComponent<GameBehavior>();
-		if(!behave.pause){
+		if(active){
+			var behave = GameObject.FindGameObjectWithTag("Behaviour").GetComponent<GameBehavior>();
+			if(!behave.pause){
 
 
-		delay = delay - Time.deltaTime;
+			delay = delay - Time.deltaTime;
 
-		if (!gameObject.GetComponent<Enemy> ().dead && delay < 0) {
+			if (!gameObject.GetComponent<Enemy> ().dead && delay < 0) {
 
-			//gets distance between enemy and player axis
-			 distanceY = Mathf.Abs (player.transform.position.y - transform.position.y);
-			 distanceX = Mathf.Abs (player.transform.position.x - transform.position.x);
+				//gets distance between enemy and player axis
+				 distanceY = Mathf.Abs (player.transform.position.y - transform.position.y);
+				 distanceX = Mathf.Abs (player.transform.position.x - transform.position.x);
 
-			//is in range
-			if(distanceX < rangeMax && distanceY < rangeMax){
+				//is in range
+				if(distanceX < rangeMax && distanceY < rangeMax){
 
-				//should move
-				if((distanceX > rangeMin && distanceY > rangeMin))
-				{
-					gameObject.GetComponent<Enemy> ().isShooting = false;
+					//should move
+					if((distanceX > rangeMin && distanceY > rangeMin) || (rangeShoot < distanceX || rangeShoot < distanceY))
+					{
+						gameObject.GetComponent<Enemy> ().isShooting = false;
 
-//						print(distanceX);
-//						print(distanceY);
-					//check wich one is closer to move in that direction
-					if(distanceX < distanceY || distanceY < rangeMin){
-						//move in x axis
-						//check if left or right
-						if(player.transform.position.x > transform.position.x)
-						{
-							//move right
-							transform.Translate(Vector3.right * speed * Time.deltaTime);
-							direction = 1;
+	//						print(distanceX);
+							print("mover");
+	//						print(distanceY);
+						//check wich one is closer to move in that direction
+						if((distanceX < distanceY && rangeShoot > distanceY) || distanceY < rangeMin){
+							//move in x axis
+							//check if left or right
+								print("mover x");
+
+							if(player.transform.position.x > transform.position.x)
+							{
+								//move right
+								transform.Translate(Vector3.right * speed * Time.deltaTime);
+								direction = 1;
+
+							}
+							if(player.transform.position.x < transform.position.x)
+							{
+								//move left
+								transform.Translate(Vector3.left * speed * Time.deltaTime);
+								direction = 3;
+
+
+							}
+
+
+						}else{
+							//move in y axis
+							//check if up or down
+
+							if(player.transform.position.y > transform.position.y)
+							{
+								//move up
+								transform.Translate(Vector3.up * speed * Time.deltaTime);
+								direction = 0;
+
+							}
+							if(player.transform.position.y < transform.position.y)
+							{
+								//move down
+								transform.Translate(Vector3.down * speed * Time.deltaTime);
+								direction = 2;
+
+							}
+							
 
 						}
-						if(player.transform.position.x < transform.position.x)
-						{
-							//move left
-							transform.Translate(Vector3.left * speed * Time.deltaTime);
-							direction = 3;
 
 
-						}
 
 
+						legAnimator.SetBool("IsRunning", true);
 					}else{
-						//move in y axis
-						//check if up or down
-						if(player.transform.position.y > transform.position.y)
-						{
-							//move up
-							transform.Translate(Vector3.up * speed * Time.deltaTime);
-							direction = 0;
+					//should only look to player
+						legAnimator.SetBool("IsRunning", false);
+
+
+						if(distanceX < distanceY){
+						//same x should check y
+							if(player.transform.position.y > transform.position.y)
+							{
+								//look up
+								direction = 0;
+							}
+							if(player.transform.position.y < transform.position.y)
+							{
+								//look down
+								direction = 2;
+							}
+						}else{
+							//same y should check x
+							if(player.transform.position.x > transform.position.x)
+							{
+								//look right
+								direction = 1;
+							}
+							if(player.transform.position.x < transform.position.x)
+							{
+								//look left
+					 			direction = 3;
+							}
 
 						}
-						if(player.transform.position.y < transform.position.y)
-						{
-							//move down
-							transform.Translate(Vector3.down * speed * Time.deltaTime);
-							direction = 2;
 
-						}
-						
+						gameObject.GetComponent<Enemy> ().isShooting = true;
+						delay = 1;
 
 					}
+					gameObject.GetComponent<Enemy> ().shootDirection = direction;
+					bothAnimator.SetInteger ("direction", direction);
+					torsoAnimator.SetInteger ("Direction", direction);
+					legAnimator.SetInteger ("Direction", direction);
 
-
-
-
-					legAnimator.SetBool("IsRunning", true);
-				}else{
-				//should only look to player
-					legAnimator.SetBool("IsRunning", false);
-
-
-					if(distanceX < distanceY){
-					//same x should check y
-						if(player.transform.position.y > transform.position.y)
-						{
-							//look up
-							direction = 0;
-						}
-						if(player.transform.position.y < transform.position.y)
-						{
-							//look down
-							direction = 2;
-						}
-					}else{
-						//same y should check x
-						if(player.transform.position.x > transform.position.x)
-						{
-							//look right
-							direction = 1;
-						}
-						if(player.transform.position.x < transform.position.x)
-						{
-							//look left
-				 			direction = 3;
-						}
-
-					}
-					gameObject.GetComponent<Enemy> ().isShooting = true;
-					delay = 1;
 
 				}
-				gameObject.GetComponent<Enemy> ().shootDirection = direction;
-				bothAnimator.SetInteger ("direction", direction);
-				torsoAnimator.SetInteger ("Direction", direction);
-				legAnimator.SetInteger ("Direction", direction);
+
+
+			
+			}
+			}else{
+
+				legAnimator.SetBool("IsRunning", false);
 
 
 			}
 
 
-		
 		}
-		}else{
-
-			legAnimator.SetBool("IsRunning", false);
-
-
-		}
-
-
-
 	}
 
 }
---
+++ Bad Barry/Assets/Script/EnemyScripts/VisionColision.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionColision : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = gameObject.GetComponentInParent<FollowPlayer>();
+
+		var rotationVector = transform.rotation.eulerAngles;
+		rotationVector.z = x.direction * -90;
+		transform.rotation = Quaternion.Euler(rotationVector);
+
+
+	
+	}
+
+
+	void OnTriggerEnter2D (Collider2D col){
+
+
+		if(col.gameObject.tag == "Player"){
+
+			var x = gameObject.GetComponentInParent<FollowPlayer>();
+			x.active = true;
+		}
+
+
+
+	}
+
+
+
+}
