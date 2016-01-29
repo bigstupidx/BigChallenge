6e158e421ca68a08b7d5bf9210bffe384ef9087d
Lucas Bonin <lucasbonin369@gmail.com>
2016-01-29 13:44:33 -0200

    Credits

---
+++ Bad Barry/Assets/AutoScroll.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoScroll : MonoBehaviour {
+	private Transform content;
+	private float timer=0;
+	private float speed=0.001f;
+	private bool enableScrool = true;
+	private float distanceToScrool = 1f;
+	private GameBehavior behave;
+	// Use this for initialization
+	void Start () {
+		content = gameObject.transform;
+		behave = GameObject.FindGameObjectWithTag("Behaviour").GetComponent<GameBehavior>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (enableScrool){
+			if (timer < speed) {
+				timer += Time.deltaTime;
+			} else {
+				timer = 0;
+				content.position = new Vector3 (transform.position.x, transform.position.y + distanceToScrool, transform.position.z);
+			}
+		}
+	}
+
+	public void goToMap(AudioSource audio){
+		behave.GoToMapWithSound (audio);
+	}
+}
